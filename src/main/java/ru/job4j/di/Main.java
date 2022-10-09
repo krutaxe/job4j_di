7947f1bcdf1f99a664f4122bd2ec289ccbcf9d99
krutaxe <krutaxe@mail.ru>
2022-10-09 21:23:40 +0300

    0. Что такое DI? [#6876]

---
+++ src/main/java/ru/job4j/di/Main.java
@@ -0,0 +1,20 @@
+package ru.job4j.di;
+
+public class Main {
+    public static void main(String[] args) {
+        Context context = new Context();
+
+        context.reg(Store.class);
+        context.reg(ConsoleInput.class);
+        context.reg(StartUI.class);
+
+        StartUI ui = context.get(StartUI.class);
+        ConsoleInput ci = context.get(ConsoleInput.class);
+
+        ui.add("Petr Arsentev");
+        ui.add("Dima Nikolaev");
+        ui.add(ci.askStr("You name?"));
+        ui.print();
+
+    }
+}
